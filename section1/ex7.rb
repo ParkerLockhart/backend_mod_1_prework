d7c9bc333556a469e0386f57c8ffc9d1d401ab88
Parker Lockhart <parkerlockhart42@gmail.com>
2021-08-26 12:49:39 -0500

    Add comments

--- section1/ex7.rb
+++ section1/ex7.rb
@@ -1,8 +1,13 @@
+#prints a string
 puts "Mary had a little lamb."
+#prints a string with another string inside
 puts "Its fleece was white as #{'snow'}."
+#prints a string
 puts "And everywhere that Mary went."
-puts "." * 10 #what'd that do?
+#prints a string, repeated ten times
+puts "." * 10
 
+#defines variables with a string
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,7 +21,11 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-#watch that print vs. puts on this line what's it do?
 
+#prints a variable, followed by other variables (all strings)
 print end1 + end2 + end3 + end4 + end5 + end6
+#also prints a variable, followed by other variables (all strings as well)
 puts end7 + end8 + end9 + end10 + end11 + end12
+
+#I remember that puts usually separates the printout by a new line (as opposed to print), but maybe this time the variables added overrides that, versus what would happen if you used puts separately with each var ?
+#I'll have to play around with it 
