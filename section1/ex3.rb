564326c17dd344c524a906b99e0e8f6c7b2c49cf
Parker Lockhart <parkerlockhart42@gmail.com>
2021-08-26 09:33:13 -0500

    Add file ex3.rb

---
+++ section1/ex3.rb
@@ -0,0 +1,37 @@
+#prints a text string
+puts "I will now count my chickens:"
+
+#prints a text string with the result of a math computation inside
+puts "Hens #{25 + 30 / 6}"
+#prints a text string with the result of a math computation inside
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#prints a text string
+puts "Now I will count the eggs:"
+
+#prints the result of a math computation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#prints a text string, no computation here
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#does a computation and returns a boolean
+puts 3 + 2 < 5 - 7
+
+#prints a text string with the result of a math computation inside
+puts "What is 3 + 2? #{3 + 2}"
+#prints a text string with the result of a math computation inside
+puts "What is 5 - 7? #{5 - 7}"
+
+#prints a text string
+puts "Oh, that's why it's false."
+
+#prints a text string
+puts "How about some more."
+
+#does a computation and returns a boolean
+puts "Is it greater? #{5 > -2}"
+#does a computation and returns a boolean
+puts "Is it greater or equal? #{5 >= -2}"
+#does a computation and returns a boolean 
+puts "Is it less or equal? #{5 <= -2}"
