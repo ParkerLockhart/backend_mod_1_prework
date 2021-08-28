e2320cf0176b5a96295bd07de348157ca45b957b
Parker Lockhart <parkerlockhart42@gmail.com>
2021-08-28 13:17:18 -0500

    Complete exercise

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,38 +11,56 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+quote = "Harry Potter must not return to Hogwarts!"
+p quote
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
+students = students + 2
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
+students = students - 2
 p students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Parker"
+is_hungry = true
+number_of_pets = 3
+
+p first_name
+p is_hungry
+p number_of_pets
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
-# Explain.
+# Explain:
+
+# I assumed from the name of the variable that first_name should store a name, which would be a string. The variable is_hungry seemed like it should store a boolean because the variable name infers its storing a binary state.
+#The variable name number_of_pets seems to be suggesting that the data type should be an integer because it refers to the number of something.
 
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
+first_name = "Sam"
+is_hungry = false
+number_of_pets = 1000
+
+p first_name
+p is_hungry
+p number_of_pets
 
 
 # YOU DO:
@@ -50,10 +68,14 @@ p students
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
+p healthy_snacks + junk_food_snacks
+
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
+
+  #yes! It all turned out well. 
